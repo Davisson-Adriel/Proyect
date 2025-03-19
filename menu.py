8f9aef9aa5221b5b160bc0b97a3c1ca4b6adb0bf
Davisson-Adriel <droman819@unab.edu.co>
2025-03-19 13:39:36 -0500

    feat: función menú principal y selección

---
+++ menu.py
@@ -0,0 +1,23 @@
+from utilidades import*
+print("--------------------------------------------------")
+print("Bienvenido a Digital Delicias Casera")
+
+while True:
+
+    mostrar_menu()
+    opc=pedir_opc()
+
+    if opc==1:
+        print("")
+    elif opc==2:
+        print("")
+    elif opc==3:
+        print("")
+    elif opc==4:
+        print("")
+        print("Hasta luego querido usuario")
+        break
+    else:
+        print("")
+        print("Opciòn invalida, intente nuevamente")
+
---
+++ utilidades.py
@@ -0,0 +1,11 @@
+def mostrar_menu():
+    print("--------------------------------------------------")
+    print(" ")
+    print("1. Registro de productos\n2. Pedidos\n3. Consultas")
+    print(" ")
+    print("--------------------------------------------------")
+
+def pedir_opc():
+    print("")
+    opc=int(input("Digite la opciòn deseada: "))
+    return opc
